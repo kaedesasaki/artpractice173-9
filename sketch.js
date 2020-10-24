3ba7f152d97293a0a4c7e7b4f227c0f09319fa87
kaedesasaki <71185154+kaedesasaki@users.noreply.github.com>
2020-10-25 01:43:19 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-var anim_kiki, anim_lala;
+var anim_kiki, anim_lala, anim_star, anim_star2;
 
 function preload(){
   anim_kiki = loadAnimation("assets/kiki1.png","assets/kiki4.png");
@@ -14,6 +14,12 @@ function preload(){
 	
 function setup(){
   createCanvas(1000, 1000);
+	for (var i=0 ; i < 7 ; i++){
+    var sprite = createSprite(random(width), random(-height)); //座標を上方向、左右にずらす
+    sprite.addAnimation("star", "assets/star1.png", "assets/star4.png");
+    sprite.velocity.y = 1; //落ちてくる速度(y軸方向の速度) 
+  }
+	
   var sprite = createSprite(300, 630);
   sprite.addAnimation("kiki", anim_kiki); 
   sprite = createSprite(580, 780); 
@@ -21,7 +27,7 @@ function setup(){
   sprite = createSprite(800, 200); 
   sprite.addAnimation("star", anim_star);
   sprite = createSprite(600, 300); 
-  sprite.addAnimation("star", anim_star2);
+  sprite.addAnimation("star2", anim_star2);
 }
 
 function draw(){
